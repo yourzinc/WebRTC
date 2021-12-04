2bc139d1811ae5f4d80b16edb0912f43b295029b
woozoo_zinc <your_zinc@ewhain.net>
2021-12-04 17:57:45 +0900

    [21-12-04] server.js - new roomID를 DB에 저장하기

--- server.js
+++ server.js
@@ -18,11 +18,23 @@ con.connect(function(err){
     console.log('Connected');
 
     //DB 생성 코드
+
     // con.query('CREATE DATABASE express_db', function(err, result){
     //     if(err) throw err;
     //     console.log('database created');
     // })
-})
+
+    //DB의 TABLE 생성 코드
+
+    // const sql = 'CREATE TABLE users (userID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, roomID VARCHAR(255) NOT NULL)';
+    // con.query(sql, function (err, result)
+    // {
+    //     if(err) throw err;
+    //     console.log('Table Created');
+    // });
+
+});
+
 /*============================================================*/
 
 const fs = require('fs');
@@ -42,10 +54,23 @@ const io = require('socket.io')(server);
 var url = new Array(5);
 var i =0;
 
+// USER ID
+// var j =0;
+// var new_user_id = j;
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
  
 app.get('/', (req, res) => {
+
+    // SELECT문 - DB에 저장된 정보 조회하기
+
+    // const sql = "select * from users";
+    // con.query(sql, function(err, result, fields) {
+    //     if(err) throw err;
+    //     console.log(result);
+    // })
+
     res.render('enter',{url_info:url})
     //res.redirect(`/${uuidV4()}`) // `` VS ''
                                  // Random uuid(Room Number)
@@ -55,6 +80,17 @@ app.get('/', (req, res) => {
 app.get('/makeRoom',(req,res)=>{
     
     url[i]=uuidV4()
+
+    // INSERT문 - NEW roomID를 DB에 저장하기
+
+    const sql = "INSERT INTO users(roomID) VALUES(?)"
+
+    con.query(sql, [url[i]], function(err, result, fields){
+        if(err) throw err;
+        console.log(result);
+    })
+    //
+
     res.redirect(`/${url[i]}`) 
     i+=1;
 })
@@ -66,6 +102,8 @@ app.get('/enterRecentRoom',(req,res)=>{
 
 
 app.get('/enterRoom_1',(req,res)=>{
+
+    
     res.redirect(`/${url[0]}`)
 })
 app.get('/enterRoom_2',(req,res)=>{
